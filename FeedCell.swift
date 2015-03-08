b8bd7ea64fe170f9f32eb955ac5548738e4582f2
mrabins <markrabins@gmail.com>
2015-03-07 23:22:32 -0800

    Initial Commit

---
+++ FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Mark Rabins on 3/7/15.
+//  Copyright (c) 2015 self.swift. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
