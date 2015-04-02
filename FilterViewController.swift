55d9efdfe942b6038d77cdbf6ac1d72f4aad0585
mrabins <markrabins@gmail.com>
2015-04-02 11:34:39 -0700

    Implement filters

--- FilterViewController.swift
+++ FilterViewController.swift
@@ -14,6 +14,10 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     var collectionView: UICollectionView!
     
     let kIntensity = 0.7
+    
+    var context:CIContext = CIContext(options: nil)
+    
+    var filters:[CIFilter] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +34,8 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         collectionView.registerClass(FilterCell.self, forCellWithReuseIdentifier: "MyCell")
     
         self.view.addSubview(collectionView)
+        
+        filters = photoFilters()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,34 +46,31 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     // UICollectionViewDataSource
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return filters.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell:FilterCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as FilterCell
-        cell.imageView.image = UIImage(named: "Placeholder")
+//        cell.imageView.image = UIImage(named: "Placeholder")
+        
+        cell.imageView.image = filteredImageFromImage(thisFeedItem.image, filter: filters[indexPath.row])
+        
         return cell
     }
     
     // Helper Function
     
-    func photoFilters() -> [CIFilter] {
-        
-        let blur = CIFilter(name: "CIGassianBlur")
-        
+    func photoFilters () -> [CIFilter] {
+        let blur = CIFilter(name: "CIGaussianBlur")
         let instant = CIFilter(name: "CIPhotoEffectInstant")
-        
         let noir = CIFilter(name: "CIPhotoEffectNoir")
-        
         let transfer = CIFilter(name: "CIPhotoEffectTransfer")
-        
         let unsharpen = CIFilter(name: "CIUnsharpMask")
-        
         let monochrome = CIFilter(name: "CIColorMonochrome")
         
         let colorControls = CIFilter(name: "CIColorControls")
-        colorControls.setValue(7.5, forKey: kCIInputSaturationKey)
+        colorControls.setValue(0.5, forKey: kCIInputSaturationKey)
         
         let sepia = CIFilter(name: "CISepiaTone")
         sepia.setValue(kIntensity, forKey: kCIInputIntensityKey)
@@ -77,14 +80,29 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
         
         let composite = CIFilter(name: "CIHardLightBlendMode")
-        composite.setValue(sepia.outputImage, forKey: kCIInputIntensityKey)
+        composite.setValue(sepia.outputImage, forKey: kCIInputImageKey)
         
         let vignette = CIFilter(name: "CIVignette")
         vignette.setValue(composite.outputImage, forKey: kCIInputImageKey)
+        
         vignette.setValue(kIntensity * 2, forKey: kCIInputIntensityKey)
         vignette.setValue(kIntensity * 30, forKey: kCIInputRadiusKey)
         
-        return [blur, noir, transfer, unsharpen, monochrome, colorControls, sepia, colorControls, colorClamp, vignette, composite]
+        return [blur, instant, noir, transfer, unsharpen, monochrome, colorControls, sepia, colorClamp, composite, vignette]
+    }
+    
+    func filteredImageFromImage (imageData: NSData, filter: CIFilter) -> UIImage {
+        let unfilteredImage = CIImage(data: imageData)
+        filter.setValue(unfilteredImage, forKey: kCIInputImageKey)
+        let filteredImage:CIImage = filter.outputImage
+        
+        let extent = filteredImage.extent()
+        let cgImage:CGImageRef = context.createCGImage(filteredImage, fromRect: extent)
+        
+        let finalImage = UIImage(CGImage: cgImage)
+        
+        return finalImage!
+        
     }
 
 }
