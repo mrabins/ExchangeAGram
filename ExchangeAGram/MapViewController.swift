21fdc2b8770ad20195733be5ac4e96015fe29db8
mrabins <markrabins@gmail.com>
2015-05-31 22:29:52 -0700

    Update map to be dynamic

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 import MapKit
-
-
+import CoreData
 
 class MapViewController: UIViewController {
 
@@ -20,17 +19,24 @@ class MapViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegionMake(location, span)
-        mapView.setRegion(region, animated: true)
+//        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        let region = MKCoordinateRegionMake(location, span)
+//        mapView.setRegion(region, animated: true)
+//        
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = location
+//        annotation.title = "Canal Saint-Martin"
+//        annotation.subtitle = "Paris"
+//        
+//        mapView.addAnnotation(annotation)
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Canal Saint-Martin"
-        annotation.subtitle = "Paris"
-        
-        mapView.addAnnotation(annotation)
+        let request = NSFetchRequest(entityName: "FeedItem")
+        let appDelegate: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext!
+        var error:NSError?
+        let itemArray = context.executeFetchRequest(request, error: &error)
+        println(error)
     }
 
     override func didReceiveMemoryWarning() {
